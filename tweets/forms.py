479052044abd531a929f18eff2eb206a63d7f682
stapleasset11 <mwavulalenjo@gmail.com>
2024-10-06 12:21:40 +0300

    added some stuff

---
+++ tweets/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Tweet
+
+MAX_TWEET_LENGTH = 50
+
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = ["content"]
+
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if len(content) > MAX_TWEET_LENGTH:
+            raise forms.ValueError("The tweet exceeds the required character limit")
+        return content
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,48 @@
+import random
 from django.shortcuts import render
+from django.http import HttpResponse,Http404,JsonResponse
+from .models import Tweet
+from .forms import TweetForm
 
 # Create your views here.
+def home_view(request,*args,**kwargs):
+    return render(request, "pages/home.html",context={},status=200)
+
+def tweet_create_view(request,*args,**kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        form = TweetForm()
+    return render(request,'components/form.html',context={"form":form}) 
+
+def tweet_list_view(request,*args,**kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id":x.id,"content":x.content,"likes":random.randint(0,100)} for x in qs]
+
+    data = {
+        "isUser":False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
+def tweet_detail_view(request,tweet_id,*args,**kwargs):
+    """
+    REST API VIEW
+    Consume by javascript or others
+    Return Json data
+    """
+    data = {
+       "id":tweet_id,
+       
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
+    except:
+        data['message'] = "Not Found"
+        status = 404
+
+    
+    return JsonResponse(data,status=status)
